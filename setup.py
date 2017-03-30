86ed282ac96799120fa03a9038804a6d60ce643d
Krylov.YS <krylov.ys@delta.ru>
2017-03-30 12:03:39 +0300

    0.11

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import find_packages
 
 setup(
     name='normy',
-    version='0.1',
+    version='0.11',
     packages=find_packages(exclude=['tests*']),
     install_requires=['pyodbc'],
     url='https://github.com/shadrus/normy',
